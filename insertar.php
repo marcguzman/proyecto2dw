9205f4b2394af0a06c1c0caedff6f68793955d24
Jorge Sandoval <jorgesandovalsuria@gmail.com>
2017-08-21 19:22:32 -0600

    Modificaciones al codigo

--- insertar.php
+++ insertar.php
@@ -25,8 +25,25 @@ else{
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Bajas</title>
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/menu.css">
+        <link rel="stylesheet" href="css/banner.css">
     </head>
+    <br>
+    <div class="topnav" id="myTopnav">
+        <a href="index.php">Menu</a>
+        <a href="Formulario.php">Formulario</a>
+        <a href="#contact">Contacto</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </div> 
+     <header>
+        <div class="contenedor">
+        <h1 class="">Registro de Usuarios</h1>
+        <nav class="menu">
+        </nav>
+    </header>
     <body>
     <center>
         <a href="Formulario.php">Regresar al Formulario</a>
