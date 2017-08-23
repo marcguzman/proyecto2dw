cb9b892c1f9c79d7df3da0cabfb47211b073bc72
Mak <Mak@Mak-PC>
2017-08-23 12:28:11 -0600

    se hizo la pagina de contacto

---
+++ contactosend.php
@@ -0,0 +1,48 @@
+<?php
+    
+    //echo 'Datos insertados satisfactoriamente';
+	echo '<script language="javascript">alert("Gracias por contactarnos, en breve nos comunicamos");</script>'; 
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CONTACTO...</title>
+        <link rel="stylesheet" href="css/fontello.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/menu.css">
+        <link rel="stylesheet" href="css/banner.css">
+        <link rel="stylesheet" href="css/3dfont.css">
+    </head>
+    <br>
+  <!--  <div class="topnav" id="myTopnav">
+        <a href="index.php">Menu</a>
+        <a href="Formulario.php">Formulario</a>
+        <a href="#contact">Contacto</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </div> -->
+     <header>
+        <div class="contenedor">
+        <h1 class="">Datos enviados...</h1>
+      <!-- <nav class="menu">
+        </nav> -->
+    </header>
+    <body>
+        <br><br>
+        
+        <!-- <center><img src="images/New.jpg" width="600" height="450">
+         <h2>Usuario agregado correctamente</h2>
+         </center>
+        <br>  -->
+    <section class="color-1">
+     <center>
+         
+      <nav class="cl-effect-1">
+        <a href="index.php">Regreso a Menu</a>
+        
+        </nav>
+    </center>
+    </section>  
+    </body>
+</html>
